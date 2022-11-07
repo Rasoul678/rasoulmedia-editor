e119190500d30c1792c05cd351c5fea8aefba3cf
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-11-07 18:06:56 +0330

    loading wasm file from unpkg.com

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
   const startService = async () => {
     await esbuild.initialize({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm/esbuild.wasm",
     });
   };
 
