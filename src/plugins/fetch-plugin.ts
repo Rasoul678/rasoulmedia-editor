4744e86af264ab569acdfeb78f99040d9555b6b9
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-11-07 17:59:46 +0330

    de-duplicate onLoad and caching logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -23,7 +23,34 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: OnLoadArgs) => {
+        const { data, request } = await axios.get(args.path);
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+        `;
+
+        const result: OnLoadResult = {
+          contents,
+          loader: "jsx",
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: OnLoadArgs) => {
         const { data, request } = await axios.get(args.path);
 
         const result: OnLoadResult = {
