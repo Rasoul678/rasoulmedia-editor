c8b1b1ab9fbd139869658dd744ee983b084a249c
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2022-11-07 12:27:04 +0330

    de-couple onResolves

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -15,22 +15,21 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: PluginBuild) {
+      //* Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      //* Handle relative paths in a module
+      build.onResolve({ filter: /\.+\// }, (args: OnResolveArgs) => {
+        return {
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+          namespace: "a",
+        };
+      });
+
+      //* Handle main file of a module
       build.onResolve({ filter: /.*/ }, (args: OnResolveArgs) => {
-        if (args.path === "index.js") {
-          return {
-            path: args.path,
-            namespace: "a",
-          };
-        }
-
-        if (args.path.includes("./" || args.path.includes("../"))) {
-          return {
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-              .href,
-            namespace: "a",
-          };
-        }
-
         return {
           path: `https://unpkg.com/${args.path}`,
           namespace: "a",
